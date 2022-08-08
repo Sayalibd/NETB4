1a90647b96552d8245f81e7566408916f069bd24
HP <HP@192.168.0.8>
2022-08-08 15:54:03 +0530

    By sam

---
+++ Final1/src/com/demo/final_a1.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class final_a1 
+{
+	public void m1()
+	{
+	System.out.println("Method_m1");
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+		
+
+	}
+}
